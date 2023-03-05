6388eddcf2941bd65ced082c13f96470ec844b31
Gesugao-san <gesugao.san@gmail.com>
2023-03-05 23:23:59 +0300

    Code and name optimisation

--- tools/semiauto.js
+++ tools/semiauto.js
@@ -5,8 +5,8 @@
 const fs = require('fs');
 const path = require('path');
 const base_path = __dirname || process.cwd();
-const target_path1 = path.resolve(base_path + '\\..\\data\\semiauto_part1.dot');
-const target_path2 = path.resolve(base_path + '\\..\\data\\semiauto_part2.json');
+const target_path1 = path.resolve(base_path + '\\..\\data\\semiauto_edges.dot');
+const target_path2 = path.resolve(base_path + '\\..\\data\\semiauto_edges.json');
 
 const local_debug = Boolean ( 0 );
 const local_verbose = Boolean ( 0 );
@@ -20,16 +20,19 @@ async function main() {
 	console.log('target_path1:', target_path1);
 	console.log('target_path2:', target_path2);
 
-	let data = fs.readFileSync(target_path1, {encoding: 'utf8', flag: 'r'});
-	data = data.replaceAll('\r', '').split('\n');
-	let data_out = [];
+	// projects in dot
+	let projs_dot = fs.readFileSync(target_path1, {encoding: 'utf8', flag: 'r'});
+	projs_dot = projs_dot.replaceAll('\r', '').split('\n');
+	let projs_dict = [];
 
 	// https://stackoverflow.com/a/18159249/8175291
-	for (let i = 0; i < data.length; i++) {
-		if (!data[i].includes("->")) continue;
-		if (data[i].trim().startsWith("//")) continue;
+	for (let i = 0; i < projs_dot.length; i++) {
+		if (!projs_dot[i].includes("->")) continue;
+		if (projs_dot[i].trim().startsWith("///")) continue;
+		if (projs_dot[i].trim().startsWith("//")) continue;
 
-		let tmp = data[i]
+		// single project
+		let proj_arr = projs_dot[i]
 			.trim()
 			//.replaceAll('  ', ' ')
 			//.replaceAll(' ', '')
@@ -37,35 +40,36 @@ async function main() {
 			.replace(' -> ', '; ')
 			.replace(' // ', '; "')
 			.split('; ');
-		for (let ii = 0; ii < tmp.length; ii++) {
-			let tmp_str = Array.from(tmp[ii]);
-			let tmp_clean = false;
-			if (tmp[ii].includes('[')) {
-				console.log('cleaning:', tmp[ii]);
-				tmp[ii] = tmp[ii].split(' [')[0];
-				tmp_str = Array.from(tmp[ii]);
-				tmp_clean = true;
+		for (let ii = 0; ii < proj_arr.length; ii++) {
+			let proj_str = Array.from(proj_arr[ii]);
+			let proj_clean = false;
+			if (proj_arr[ii].includes('[')) {
+				console.log('cleaning:', proj_arr[ii]);
+				proj_arr[ii] = proj_arr[ii].split(' [')[0];
+				proj_str = Array.from(proj_arr[ii]);
+				proj_clean = true;
 			}
-			if (tmp_str[0] == '"') {
-				tmp[ii] = tmp[ii].slice(1);
+			if (proj_str[0] == '"') {
+				proj_arr[ii] = proj_arr[ii].slice(1);
 			}
-			if (tmp_str[tmp_str.length-1] == '"') {
-				tmp[ii] = tmp[ii].slice(0, -1);
+			if (proj_str[proj_str.length - 1] == '"') {
+				proj_arr[ii] = proj_arr[ii].slice(0, -1);
 			}
-			if (tmp_clean) {
-				console.log('result:   ', tmp[ii]);
+			if (proj_clean) {
+				console.log('result:   ', proj_arr[ii]);
 			}
 		}
-		data_out.push({
-			"parrent": tmp[0],
-			"name": tmp[1],
-			"notes": (tmp.length > 3 ? tmp.slice(2, tmp.length) : tmp[2])
+		projs_dict.push({
+			"parent": proj_arr[0],
+			"name": proj_arr[1],
+			"notes": (proj_arr.length > 3 ? JSON.stringify(proj_arr.slice(2, proj_arr.length)) : proj_arr[2])
 		});
+		//
 	}
 
-	//console.log('data_out:', data_out);
+	//console.log('projs_dict:', projs_dict);
 
-	fs.writeFileSync(target_path2, JSON.stringify(data_out, null, '\t'), 'utf8');
+	fs.writeFileSync(target_path2, JSON.stringify(projs_dict, null, '\t'), 'utf8');
 
 	console.log('bye');
 	return 0;
